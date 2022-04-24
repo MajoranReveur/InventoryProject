2fec87852cb6ec105529d225f5c0dab1d32e5a77
Leandre Perrot <leandre.perrot@epita.fr>
2022-04-24 16:35:41 +0200

    Datas are now separated in two tables, depending if there all items from a gtin are still edible or no

--- InventoryProject/Inventory/views.py
+++ InventoryProject/Inventory/views.py
@@ -3,14 +3,20 @@ from .models import Food
 from .forms import FoodForm
 from django.db.models import Min
 
+import datetime
+
+peremption_sort = False
 # Create your views here.
 
 def form_view(request):
-    inv = Food.objects.values('gtin',).annotate(peremption=Min('peremption'),).order_by()
+    inv_before = Food.objects.values('gtin',).annotate(peremption=Min('peremption'),).order_by('peremption').filter(peremption__lt=datetime.date.today())
+    inv_after = Food.objects.values('gtin',).annotate(peremption=Min('peremption'),).order_by('peremption').filter(peremption__gte=datetime.date.today())
     form = FoodForm()
     if request.method == 'POST':
         form = FoodForm(request.POST)
         if form.is_valid():
             form.save()
         return redirect('/')
-    return render(request,'star/Home.html',{'form':form, 'inv': inv})
+    if(request.GET.get('mybtn')):
+        peremption_sort = True
+    return render(request,'star/Home.html',{'form':form, 'inv_before': inv_before, 'inv_after': inv_after})
