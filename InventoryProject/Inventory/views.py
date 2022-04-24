3be276256872b1271f6ceb3ee4d2e52cc8e4b745
Leandre Perrot <leandre.perrot@epita.fr>
2022-04-24 08:05:39 +0200

    The app now show the shortest expiry date for each gtin code

--- InventoryProject/Inventory/views.py
+++ InventoryProject/Inventory/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, redirect
 from .models import Food
 from .forms import FoodForm
+from django.db.models import Min
 
 # Create your views here.
 
 def form_view(request):
-    inv = Food.objects.all()
+    inv = Food.objects.values('gtin',).annotate(peremption=Min('peremption'),).order_by()
     form = FoodForm()
     if request.method == 'POST':
         form = FoodForm(request.POST)
